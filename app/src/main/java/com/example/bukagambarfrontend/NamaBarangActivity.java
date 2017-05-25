d6b459769ca09c2ffa8c4fbd88dd6a4491b01823
hinovita <novitahidayati52@gmail.com>
2017-05-25 14:20:22 +0700

    Penambahan layout, activity dialog rejected dan dialog_accepte

--- app/src/main/java/com/example/bukagambarfrontend/NamaBarangActivity.java
+++ app/src/main/java/com/example/bukagambarfrontend/NamaBarangActivity.java
@@ -1,6 +1,7 @@
 package com.example.bukagambarfrontend;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
